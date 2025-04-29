b8d4273483093c76b4dfe07244532d2b8f1c24bd
Nova <heetask@gmail.com>
2025-04-29 15:39:42 +0900

    블로그 메인 UI 1

--- notion-blog-nextjs/app/blog/page.tsx
+++ notion-blog-nextjs/app/blog/page.tsx
@@ -3,7 +3,7 @@ import Footer from '@/components/layouts/Footer';
 
 export default function Blog() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container py-8">
       <div className="space-y-8">
         {/* 섹션 제목 */}
         <h2 className="text-3xl font-bold tracking-tight">블로그</h2>
